07bb452f6a14d8b775274a486e4fd62f8df91a4f
leewootak <leewt32414@gmail.com>
2025-06-11 03:16:59 +0900

    [Feat] 문자열 섞기 (Lv.0)
    하단에 더 좋아 보이는 풀이 추가

--- Programmers/Q11.cs
+++ Programmers/Q11.cs
@@ -1,8 +1,35 @@
 using System;
 
-public class Solution {
-    public string solution(string str1, string str2) {
+public class Q11 {
+    // // 문자열 섞기 (내 풀이)
+    // public string Solution(string str1, string str2) 
+    // {
+    //     string answer = "";
+    //     int maxLength = str1.Length + str2.Length;
+        
+    //     for (int i = 0; i < maxLength; i++)
+    //     {
+    //         if (i % 2 == 0 && i/2 < str1.Length)
+    //         {
+    //             answer += str1[i/2];
+    //         }
+    //         else if (i % 2 == 1 && i/2 < str2.Length)
+    //         {
+    //             answer += str2[i/2];
+    //         }
+    //     }
+    //     return answer;
+    // }
+
+    // 더 나은 풀이
+    public string Solution(string str1, string str2) 
+    {
         string answer = "";
+        
+        for (int i = 0; i < str1.Length; i++)
+        {
+            answer += str1[i].ToString() + str2[i].ToString();
+        }
         return answer;
     }
 }
