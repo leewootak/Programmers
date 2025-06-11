4a2348b33c909543d8ba2b4b7a3b8e4cbc88b5cf
leewootak <leewt32414@gmail.com>
2025-06-12 01:27:33 +0900

    [Feat] flag에 따라 다른 값 반환하기

---
+++ Programmers/Q20.cs
@@ -0,0 +1,11 @@
+// using System;
+// // flag에 따라 다른 값 반환하기
+// public class Q20
+// {
+//     public int solution(int a, int b, bool flag)
+//     {
+//         int answer = 0;
+//         answer += flag ? a + b : a - b;
+//         return answer;
+//     }
+// }
