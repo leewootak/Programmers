f83cf0dc62a597d9c268bfc5c162550e13d18657
leewootak <leewt32414@gmail.com>
2025-06-11 18:53:50 +0900

    [Feat] 문자열 곱하기 (Lv.0)

---
+++ Programmers/Q13.cs
@@ -0,0 +1,11 @@
+// using System;
+
+// // 문자열 곱하기
+// public class Solution {
+//     public string solution(string my_string, int k) {
+//         string answer = "";
+//         for (int i = 0; i < k; i++)
+//             answer += my_string;
+//         return answer;
+//     }
+// }
