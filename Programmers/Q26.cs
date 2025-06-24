ecead18063cd9ca5d08c98dd0a26d83dcb4052bf
leewootak <leewt32414@gmail.com>
2025-06-24 19:42:52 +0900

    [Feat] 마지막 두 원소 (Lv.0)

---
+++ Programmers/Q26.cs
@@ -0,0 +1,23 @@
+// using System;
+// using System.Collections.Generic;
+// // 마지막 두 원소
+// public class Solution {
+//     public int[] solution(int[] num_list) {
+//         List<int> list = new List<int>(num_list);
+
+//         int a = num_list[num_list.Length - 1];
+//         int b = num_list[num_list.Length - 2];
+
+//         if (a > b)
+//         {
+//             int big = a - b;
+//             list.Insert(num_list.Length, big);
+//         }
+//         else
+//         {
+//             int small = a * 2;
+//             list.Insert(num_list.Length, small);
+//         }
+//         return list.ToArray();
+//     }
+// }
